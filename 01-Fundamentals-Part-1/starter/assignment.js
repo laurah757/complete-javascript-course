1a74d28935e0632481e017a4308a36b6dfe5302d
laurah757 <laura.hart@web.de>
2024-11-17 11:51:58 +0100

    Initial commit with course files

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,14 @@
+// Values and variables
+/*
+let country = "Germany";
+let continent = "Europe";
+let population = "83 Million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+*/
+
+
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,26 @@
+/*
+let js = "amazing";
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas";
+console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof "Laura");
+console.log(typeof 29);
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+*/
+
+let age = 30;
+age = 31;
+
+
+
+

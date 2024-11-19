d09a9fef990fac6c9887206e44badfee58c85261
laurah757 <laura.hart@web.de>
2024-11-19 19:13:39 +0100

    up to and including video 21

---
+++ 01-Fundamentals-Part-1/starter/codingchallenge.js
@@ -0,0 +1,57 @@
+// Mark and John are trying to compare their BMI (Body Mass Index), 
+// which is calculated using the formula:
+// BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+// and height in meter).
+
+// Your tasks:
+// 1. Store Mark's and John's mass and height in variables
+// 2. Calculate both their BMIs using the formula (you can even implement both
+// versions)
+// 3. Create a Boolean variable 'markHigherBMI' containing information about
+// whether Mark has a higher BMI than John.
+
+// const weightMark1 = 78;
+// const heightMark1 = 1.69;
+
+// const weightJohn1 = 92;
+// const heightJohn1 = 1.95;
+
+// const bmiCalculationMark1 = weightMark1 / heightMark1 ** 2;
+// const bmiCalculationJohn1 = weightJohn1 / heightJohn1 ** 2;
+
+// console.log(bmiCalculationMark1);
+// console.log(bmiCalculationJohn1);
+
+// const markHigherBMI1 = bmiCalculationMark1 > bmiCalculationJohn1;
+// console.log(markHigherBMI1);
+
+// //  Data 2
+
+// const weightMark2 = 95;
+// const heightMark2 = 1.88;
+
+// const weightJohn2 = 85;
+// const heightJohn2 = 1.76;
+
+// const bmiCalculationMark2 = weightMark2 / heightMark2 ** 2;
+// const bmiCalculationJohn2 = weightJohn2 / heightJohn2 ** 2;
+
+// console.log(bmiCalculationMark2);
+// console.log(bmiCalculationJohn2);
+
+// const markHigherBMI2 = bmiCalculationMark2 > bmiCalculationJohn2;
+// console.log(markHigherBMI2);
+
+// if (bmiCalculationMark1 > bmiCalculationJohn1) {
+//   console.log("Mark's BMI is higher than John's!")
+// } else {
+//   console.log("John's BMI is higher than Mark's!")
+// }
+
+// // 2 
+
+// if (bmiCalculationMark2 > bmiCalculationJohn2) {
+//   console.log(`Mark's BMI (${bmiCalculationMark2}) is higher than John's (${bmiCalculationJohn2})!`)
+// } else {
+//   console.log(`John's BMI (${bmiCalculationJohn2}) is higher than Mark's! (${bmiCalculationMark2})`)
+// }
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -18,12 +18,132 @@ javascriptIsFun = "YES!";
 console.log(javascriptIsFun);
 */
 
-let age = 30;
-age = 31;
+// let age = 30;
+// age = 31;
 
-const birthYear = 1995;
+// const birthYear = 1995;
 
 
+// Math operators
 
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageLaura = now - 1995;
+// console.log(ageJonas, ageLaura);
+
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
+
+// //  assignement operators
+
+// let x = 10 + 5;
+// x += 10; // x = x + 10
+// x *= 4; // x = x * 4
+// x++; // x + 1
+// x--; // x - 1
+// console.log(x);
+
+// // Comparison operators
+
+// console.log(ageJonas > ageLaura); // >, <, >=, <=
+// console.log(ageLaura >= 18);
+
+// const isFullAge = ageLaura >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+// operator precedence
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageLaura = now - 1995;
+// console.log(now - 1991 > now - 2018);
+
+// let x,y;
+// x = y = 25 -10 -5; // x = y = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageLaura) / 2;
+// console.log(ageJonas, ageLaura, averageAge);
+
+// Strings and template literals
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas = "I'm " + firstName + ", a " + (year - birthYear) + " year old "
+// + job + "!";
+
+// console.log(jonas);
+
+// const jonasNew = `I am ${firstName}, a ${year - birthYear} year old ${job}!`;
+
+// console.log(jonasNew);
+
+// if / else statements
+// const age = 12;
+// if (age >= 18) {
+//   console.log("Sarah can start driving lessons!");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young, wait another ${yearsLeft} years`);
+// }
+
+// const birthYear = 2012;
+
+// let century; // global(?) definieren, damit man es wieder ausgeben kann
+
+// if (birthYear <= 2000) {
+//   century = 20; // block scope
+// } else {
+//   century = 21; // block scope
+// }
+// console.log(century);
+
+// type conversion 
+
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+
+// console.log(Number(inputYear) + 18);
+
+// console.log(String(23), 23);
+
+// // type coercion
+// console.log("I am " + 23 + " years old.");
+// console.log("23" - "10" - 3);
+// console.log("23" * "2"); 
+
+// 5 falsy values: 0, "", undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+  
+// } else {
+//   console.log("You should get a job!");
+  
+// }
+
+// let height = 1.64;
+// if (height) {
+//   console.log("YAY! Height is defined");
+  
+// } else {
+//   console.log("Height is UNDEFINED");
+  
+// }
 
 
